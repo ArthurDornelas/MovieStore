1e229e04ba4fe4601f625e0ebc2322cd804b47b4
Arthur de Freitas Dornelas <arthur.dornelas@ativagr.com.br>
2024-04-10 00:34:39 -0300

    feat: Add page with dropzone functionality.

--- Client/movie-store/src/movies/add-movies/AddMovies.jsx
+++ Client/movie-store/src/movies/add-movies/AddMovies.jsx
@@ -1,5 +1,32 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Dropzone from 'react-dropzone'
+import  Button  from '@mui/material/Button';
+
 export default function AddMovies() {
     return (
-        <h1>Add Movies</h1>
+        <Card sx={{ minWidth: 400, minHeight: 300, display: 'flex',
+             justifyContent: 'center', alignItems: 'center'  }}>
+            <CardContent >
+                <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+                    {({getRootProps, getInputProps}) => (
+                        <section>
+                        <div style={{ display: 'flex', flexDirection: 'column',
+                            alignItems: 'center'  }} {...getRootProps()}>
+                            <input {...getInputProps()} />
+                            <p>Drag and drop files here</p>
+                            <p>or</p>
+                            <Button
+                                variant="contained"
+                                component="label"
+                                >
+                                Upload File
+                            </Button>
+                        </div>
+                        </section>
+                    )}
+                </Dropzone>
+            </CardContent>
+        </Card>
     );
 }
